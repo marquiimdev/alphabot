51eb255ed02670604aabf65f7371b6c2778374a1
Marcos <vinicin3000@gmail.com>
2020-06-26 20:06:49 -0300

    embed sifude

--- commands/level.js
+++ commands/level.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args, ops, database) => {
     
     dbref.once('value').then(async function(db) {
         let embed = new Discord.MessageEmbed()
-        .setAuthor(`Informações do seu nível, ${message.author.avatarURL}`, message.author.avatarURL())
+        .setAuthor(`Informações do seu nível, ${message.author.tag}`, message.author.avatarURL())
         .setDescription(`Você está no level ${db.val().level}!\nPróximo nível: (${db.val().xp}/${db.val().level*100}).`)
         .setColor("#36393F");
         message.channel.send(embed);
