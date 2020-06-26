00d9233724ec2489ebb59d3f2f3ea6b790ee3fd2
Marcos <vinicin3000@gmail.com>
2020-06-26 20:30:15 -0300

    dale

--- commands/level.js
+++ commands/level.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args, ops, database) => {
     let membro = message.mentions.users.first() || client.users.cache.get(args[0]);
-    if (!membro) membro = message.author;
+    if (!membro) membro = client.users.cache.get(message.author.id);
     // Referência de onde fica localizado.
     let dbref = database.ref(`Servidores/Levels/${message.guild.id}/${membro.id}`);
     
     dbref.once('value').then(async function(db) {
         if (db.val() == null) return message.reply("esse usuários não se encontra no meu banco de dados.");
-        
+
         let embed = new Discord.MessageEmbed()
         .setAuthor(`Informações do nível, ${membro.user.tag}`, membro.user.avatarURL())
         .setDescription(`Você está no level ${db.val().level}!\nExperiência pro próximo nível: (${db.val().xp}/${db.val().level*100}).`)
