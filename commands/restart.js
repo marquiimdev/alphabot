23e8fdb866bab5a5d9e39f4f2cacf1e0fdaa89a2
Marcos <vinicin3000@gmail.com>
2020-06-27 13:31:44 -0300

    dale

--- commands/restart.js
+++ commands/restart.js
@@ -1,6 +1,8 @@
 const config = require("../config.json");
-exports.run = (client, message, args) => {
+exports.run = (client, message, args, ops) => {
+    if (ops.active.get(message.guild.id)) ops.active.delete(message.guild.id);
     if (message.author.id !== "454275898582106115") return;
+
     console.clear();
     client.destroy()
     client.login(config.token);
