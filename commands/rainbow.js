09e4cb26c72ffaced053712a3d404b720fd319ad
Marcos <vinicin3000@gmail.com>
2020-06-27 13:14:02 -0300

    dale

--- commands/rainbow.js
+++ commands/rainbow.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    let cargo = message.guild.roles.cache.get('722944128484245635');
+    let cargo = message.guild.roles.cache.get('644181188466049034');
     setInterval(() => {
         cargo.setColor('RANDOM')
     }, 100)
