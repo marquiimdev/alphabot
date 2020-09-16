b7e475354f289397175e4286275b6d8791bb15cb
marquiimdev <69223716+marquiimdev@users.noreply.github.com>
2020-09-16 15:26:50 -0300

    Delete rainbow.js

--- commands/rainbow.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client, message, args) => {
-    let cargo = message.guild.roles.cache.get('726472510563942521');
-    setInterval(() => {
-        cargo.setColor('RANDOM')
-    }, 100)
-};
